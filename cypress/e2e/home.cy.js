6a7290d76141a508a7f61bb1f1d388757e65b7bc
gabrielaasantiago <gabrielaasantiago@outlook.com>
2022-10-03 14:54:59 -0300

    Atualizando spec de cadastro

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe('home page', () => {
-  it('app deve es ar online', () => {
+  it('app deve estar online', () => {
     cy.visit('/')
-    cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+    cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
   })
 })
